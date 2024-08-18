23db8d5d6aeb71d79daf92cdf5a3b931525fe7ec
anadykhan <aiyoun.khan@gmail.com>
2024-08-18 10:06:42 +0600

    New commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,26 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { apiError } from "../utils/apiError.js"
+import { User } from "../models/user.model.js"
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "Okay"
+    const { fullName, email, username, password } = req.body
+    console.log("email: ", email)
+
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new apiError(400, "All fields are required")
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
+
+    if(existedUser) {
+        throw new apiError(409, "User with email or username already existed")
+    }
+
+    
 })
 
-export {registerUser}
+export { registerUser }
